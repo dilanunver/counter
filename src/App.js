9282f2a4032d0b4d56c4f84ddbca7c895b0ec52e
dilanunver <dunver33@gmail.com>
2021-10-19 18:55:06 +0200

    counter-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const numberRef = useRef(0)
+
+  function increment() {
+    setNumber(number + 1)
+  }
+  function decrement() {
+    setNumber(number - 1)
+  }
+  function zero() {
+    setNumber(0)
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      numberRef.current += 1
+      setNumber(numberRef.current);
+    }, 1000)
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2 className="header">Counter App</h2>
+      <h1 className="number">{number}</h1>
+      <div className="row">
+        <button className="increment" onClick={increment}>+</button>
+        <button className="decrement" onClick={decrement}>-</button>
+      </div>
+      <div className="row2">
+        <button className="zero" onClick={zero}>Reset</button>
+        <button className="save">Save</button>
+      </div>
+      <button >auto increment</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
